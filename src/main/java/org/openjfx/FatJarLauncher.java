34cef9825b74cb526da1cab4a94712f348111d55
Trentan Healey <trentanh@gmail.com>
2021-06-20 11:37:06 +1000

    New maven and build with shade for jar compile All working Adding the run config to project lib (intelliJ)

--- src/main/java/org/openjfx/FatJarLauncher.java
+++ src/main/java/org/openjfx/FatJarLauncher.java
@@ -3,7 +3,7 @@ package org.openjfx;
 public class FatJarLauncher {
 
     // Shade, Assembly, and other plugins mess with the jar structure and ... threads?
-    // and i dont fully understand it but we require this little shim
+    // We require this little shim
     // https://stackoverflow.com/questions/52653836/maven-shade-javafx-runtime-components-are-missing
 
     public static void main(String[] args) {
